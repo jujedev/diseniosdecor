6d582fb2eefc9271b83ab2cf37375fffa704b413
Mauricio Abel Maldonado <134096302+jujedev@users.noreply.github.com>
2024-11-04 16:56:01 -0300

    add base project

---
+++ src/components/ResponsiveMenu.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+
+const ResponsiveMenu = ({ open }) => {
+    return (
+        <AnimatePresence mode="wait">
+            {
+                open && (
+                    <motion.div
+                        initial={{ opacity: 0, y: -100 }}
+                        animate={{ opacity: 1, y: 0}}
+                        exit={{ opacity: 0, y: -100 }}
+                        className="absolute top-20 left-0 w-full h-screen z-20"
+                    >
+                        <div className="text-xl font-semibold uppercase bg-primary text-white py-10 m-6 rounded-3xl">
+                            <ul className="flex flex-col justify-center items-center gap-10">
+                                <li className="hover:scale-105"><a href="/">Inicio</a></li>
+                                <li className="hover:scale-105"><a href="#">Quienes somos</a></li>
+                                <li className="hover:scale-105"><a href="#">Contáctanos</a></li>
+                                <li className="hover:scale-105"><a href="#">Productos</a></li>
+                            </ul>
+                        </div>
+                    </motion.div>
+                )
+            }
+        </AnimatePresence>        
+    )
+};
+
+export default ResponsiveMenu;
---
+++ src/mockData/data.js
@@ -0,0 +1,22 @@
+export const NavbarMenu = [
+    {
+        id: 1,
+        title: "Inicio",
+        link: "/"
+    },
+    {
+        id: 1,
+        title: "Quienes somos",
+        link: "#"
+    },
+    {
+        id: 1,
+        title: "Contactanos",
+        link: "#"
+    },
+    {
+        id: 1,
+        title: "Productos",
+        link: "#"
+    },
+]
