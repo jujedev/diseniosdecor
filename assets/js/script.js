0ce193d1403853d10b8ec90ff698c6dd2d675d4c
MqttLab <maurimaldonado6@gmail.com>
2024-11-13 18:52:12 -0300

    Cards terminadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,15 @@ const $header = document.querySelector("[data-header]");
 window.addEventListener("scroll", e => {
     $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+/**
+ * Add to favourite button toggle
+ */
+
+const $toggleBtns = document.querySelectorAll("[data-toggle-btn]");
+
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener("click", () => {
+        $toggleBtn.classList.toggle("active");
+    });
+});
